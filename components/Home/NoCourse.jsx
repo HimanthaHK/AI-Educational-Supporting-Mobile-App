305b771db9d851396787f02e13c27986391af8bb
HimanthaHK <142646361+HimanthaHK@users.noreply.github.com>
2025-02-03 21:50:09 -0800

    Add Course

--- components/Home/NoCourse.jsx
+++ components/Home/NoCourse.jsx
@@ -1,8 +1,10 @@
  import { View, Text, Image } from 'react-native'
  import React from 'react'
 import Button from '../Shared/Button'
+import { useRouter } from 'expo-router'
  
  export default function NoCourse() {
+  const router=useRouter();
    return (
      <View style={{
         marginTop:40,
@@ -18,7 +20,7 @@ import Button from '../Shared/Button'
         fontFamily:'outfit-bold',
         fontSize:20
        }}>You don't have any course</Text>
-       <Button text={'Create New Course'}/>
+       <Button text={'Create New Course'}onPress={()=>router.push('/addCourse')}/>
        <Button text={'Explore Existing Courses'}
        type='outline'/>
      </View>
