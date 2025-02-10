ca5375e326d59ec693119fbac4d7459130f6ba4e
HimanthaHK <142646361+HimanthaHK@users.noreply.github.com>
2025-02-10 03:31:06 -0800

    option-2

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,32 @@
 import { View, Text, Platform } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Header from '../../components/Home/Header'
 import Colors from '../constant/Colors'
 import NoCourse from '../../components/Home/NoCourse'
+import {db} from '../../config/firebaseConfig'
+import { UserDetailContext } from '../../context/UserDetailContext'
+import { doc, getDocs, where } from 'firebase/firestore'
 
 export default function Home() {
+
+  const{userDetail,setUserDetail}=useContext(UserDetailContext);
+
+  const [courseList,setCourseList]=useState([]);
+
+  useEffect(()=>{
+    userDetail && GetCourseList();
+   }, [userDetail])
+
+  const GetCourseList=async()=>{
+    const q=query(collection(db,'Courses'),where("createdBy",'==',userDetail?.email))
+    const querySnapshot=await getDocs(q);
+
+    querySnapshot.forEach((doc)=>{
+      console.log("---",doc.data());
+    })
+  }
+
+
   return (
     <View style={{
         padding:25,
